58baa2fa8a5358ac2293f3b53edc6936417c2cce
pra <prabhat933@gmail.com>
2019-10-11 02:33:17 +0530

    fourth commit

--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ todo-app/src/app/components/todos/todos.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  todoContent: string;
+  todos: string[] = [];
+  saveTodo() {
+    this.todos.push(this.todoContent);
+    this.todoContent = '';
+  }
+  constructor() {
+  }
+   
+  ngOnInit() {
+  }
+}
